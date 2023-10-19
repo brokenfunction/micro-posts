71a46f96dafe58a02338db2c09f8e222518a1d71
rzviazok <romahazvz@gmail.com>
2023-10-19 18:49:31 +0300

    Authentication & Authorization

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use App\Form\MicroPostType;
 use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,6 +27,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
@@ -34,6 +36,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, new MicroPost());
@@ -42,7 +45,7 @@ class MicroPostController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
-            $post->setCreated(new DateTime());
+            $post->setAuthor($this->getUser());
             $posts->add($post, true);
 
             $this->addFlash('success', 'Your micro post have been added');
@@ -56,6 +59,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/edit/{post}', name: 'app_micro_post_edit')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);
@@ -78,6 +82,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/comment/{post}', name: 'app_micro_post_comment')]
+    #[IsGranted('ROLE_COMMENTER')]
     public function addComment(MicroPost $post, Request $request, CommentRepository $comments): Response
     {
         $form = $this->createForm(CommentType::class, new Comment());
@@ -87,6 +92,7 @@ class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $comment = $form->getData();
             $comment->setPost($post);
+            $comment->setAuthor($this->getUser());
             $comments->add($comment, true);
 
             $this->addFlash('success', 'Your comment have been updated');
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,30 +3,50 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword($this->userPasswordHasher->hashPassword($user1, '123456'));
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('test1@test.com');
+        $user2->setPassword($this->userPasswordHasher->hashPassword($user2, '123456'));
+        $manager->persist($user2);
+
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Hello world');
         $microPost1->setText('lorem ipsum ...');
         $microPost1->setCreated(new DateTime());
+        $microPost1->setAuthor($user1);
         $manager->persist($microPost1);
 
         $microPost2 = new MicroPost();
         $microPost2->setTitle('Welcome');
         $microPost2->setText('lorem ipsum ...');
         $microPost2->setCreated(new DateTime());
+        $microPost2->setAuthor($user1);
         $manager->persist($microPost2);
 
         $microPost3 = new MicroPost();
         $microPost3->setTitle('Greetings');
         $microPost3->setText('lorem ipsum ...');
         $microPost3->setCreated(new DateTime());
+        $microPost3->setAuthor($user2);
         $manager->persist($microPost3);
 
         $manager->flush();
