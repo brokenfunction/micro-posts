95bea186afa79c2c764b3babe0e57d4b752178c2
rzviazok <romahazvz@gmail.com>
2023-10-23 18:20:33 +0300

    User Profiles, Likes, Following Features

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- src/Form/MicroPostType.php
+++ src/Form/MicroPostType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\MicroPost;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +16,11 @@ class MicroPostType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('text');
+            ->add('text', CKEditorType::class, [
+                'config' => [
+                    'uiColor' => '#ffffff',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
